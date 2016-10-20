d6a6e6b07d8d2f9535fccaef71fc64bdd60eed78
willnickerson <wnickers@willamette.edu>
2016-10-20 13:22:08 -0700

    refactored makedir function and make dir test

--- test/store.test.js
+++ test/store.test.js
@@ -1,21 +1,14 @@
 var store = require('../lib/store');
 var assert = require('assert');
-var storeDirectory = require('../lib/storeDirectory');
+var storeDir = require('../lib/storeDirectory');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 
 
 describe('makeDir method', () => {
   it('creates a new directory', done => {
-    mkdirp('./store', err => {
-      if(err) {
-        console.log(err);
-        done(err);
-      } else {
-        console.log('we made a directory!');
-        assert.ok(fs.existsSync('./store'));
-        done();
-      }
-    });
+    storeDir.makeDir();
+    assert.ok(fs.existsSync('./store'));
+    done();
   });
 });
