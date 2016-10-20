5a30793e2c3cf356214e88caaab3be63b24ee497
willnickerson <wnickers@willamette.edu>
2016-10-20 13:12:21 -0700

    test for makedir method

--- test/store.test.js
+++ test/store.test.js
@@ -1,2 +1,21 @@
-var store = require('../lib/store')
+var store = require('../lib/store');
+var assert = require('assert');
+var storeDirectory = require('../lib/storeDirectory');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
 
+
+describe('makeDir method', () => {
+  it('creates a new directory', done => {
+    mkdirp('./store', err => {
+      if(err) {
+        console.log(err);
+        done(err);
+      } else {
+        console.log('we made a directory!');
+        assert.ok(fs.existsSync('./store'));
+        done();
+      }
+    });
+  });
+});
